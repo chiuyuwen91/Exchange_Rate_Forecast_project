9ac60b74685c3a5e6333e410a5af968e64e75c93
Naomi <chiuyuwen91@gmail.com>
2019-10-29 21:27:37 +0800

    Update

--- model_build.py
+++ model_build.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 from keras import models
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.noise import AlphaDropout
 from keras.layers import GaussianNoise
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
 from keras.optimizers import SGD
 from sklearn.model_selection import train_test_split
+
 def build_model(n_dense=6,
                    dense_units=16,
                    activation='selu',
@@ -35,9 +39,9 @@ def build_model(n_dense=6,
     # sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.add(Dense(num_classes))
     model.add(Activation(activation))
-    model.compile(loss='mae',
-                  metrics=['mae'],
-                  optimizer=optimizer)
+    # model.compile(loss='mae',
+    #               metrics=['mae'],
+    #               optimizer=optimizer)
 
     return model
 
@@ -55,51 +59,64 @@ train_data, test_data, train_targets, test_targets = train_test_split(X, y, test
 #
 # test_data -= mean
 # test_data /= std
-
 # ++++++++++++++++K-fold validation
-k = 4
-num_val_samples = len(train_data) // k
-num_epochs = 500
-all_mae_histories = []
+# k = 4
+# num_val_samples = len(train_data) // k
+# num_epochs = 200
+# all_mae_histories = []
+#
+# for i in range(k):
+#     print('Processing fold #', i)
+#     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+#     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+#
+#     partial_train_data = np.concatenate(
+#         [train_data[: i * num_val_samples],
+#          train_data[(i + 1) * num_val_samples:]],
+#         axis=0)
+#     partial_train_targets = np.concatenate(
+#         [train_targets[: i * num_val_samples],
+#          train_targets[(i + 1) * num_val_samples:]],
+#         axis=0)
+#     model = build_model()
+#     history = model.fit(partial_train_data,
+#                         partial_train_targets,
+#                         validation_data=(val_data, val_targets),
+#                         epochs=num_epochs,
+#                         batch_size=1,
+#                         verbose=1)
+#     mae_history = history.history['val_mean_absolute_error']
+#     all_mae_histories.append(mae_history)
+#     average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
 
-for i in range(k):
-    print('Processing fold #', i)
-    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+# +++++++++++++++++ callback
+file_path = 'weights_best.hdf5'
 
-    partial_train_data = np.concatenate(
-        [train_data[: i * num_val_samples],
-         train_data[(i + 1) * num_val_samples:]],
-        axis=0)
-    partial_train_targets = np.concatenate(
-        [train_targets[: i * num_val_samples],
-         train_targets[(i + 1) * num_val_samples:]],
-        axis=0)
-    model = build_model()
-    history = model.fit(partial_train_data,
-                        partial_train_targets,
-                        validation_data=(val_data, val_targets),
-                        epochs=num_epochs,
-                        batch_size=1,
-                        verbose=1)
-    mae_history = history.history['val_mean_absolute_error']
-    all_mae_histories.append(mae_history)
-    average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
-
-
+model_ckpt = ModelCheckpoint(filepath=file_path,
+                             monitor='val_mean_absolute_error',
+                             save_best_only=True)
 # +++++++++++++++++ triaining final model
 model = build_model()
+model.summary()
+model.compile(loss='mae',
+                  metrics=['mae'],
+                  optimizer='rmsprop')
 model.fit(train_data,
           train_targets,
-          epochs=300,
+          epochs=500,
           batch_size=16,
-          verbose=1)
-model.save("final_model.h5")
-model.save_weights("model_weights.h5")
+          verbose=1,
+          callbacks=[model_ckpt])
+model.save('final_model.h5')
+model.save_weights('model_weights.h5')
 test_mae, _ = model.evaluate(test_data, test_targets)
 print('Test MAE:', test_mae)
+
+pred_final = load_model('weights_best.hdf5')
+pred_loadback = model.predict(test_data)
+print(pred_loadback)
 # +++++++++++++++++ matplotlib
 plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
 plt.xlabel('Epochs')
 plt.ylabel('Validation Mae')
-plt.savefig('C:/share_VM/work/Project_PPI/training_result_mae.png')
+plt.savefig('./info/training_result_mae.png')
