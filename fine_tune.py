84e26bc434fda6bc50a7d02916bdf1feea5596c9
Naomi <chiuyuwen91@gmail.com>
2019-10-31 17:34:27 +0800

    Update

--- fine_tune.py
+++ fine_tune.py
@@ -1,46 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from keras import models
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.noise import AlphaDropout
-from keras.layers import GaussianNoise
 from keras.models import load_model
-from keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 
 
-def build_model(n_dense=6,
-                dense_units=16,
-                activation='selu',
-                dropout=AlphaDropout,
-                dropout_rate=0.1,
-                kernel_initializer='lecun_normal',
-                optimizer='rmsprop',
-                num_classes=1, ):
-    model = models.Sequential()
-
-    model.add(Dense(dense_units,
-                    input_shape=(train_data.shape[1],),
-                    kernel_initializer=kernel_initializer,
-                    ))
-    model.add(Activation(activation))
-    model.add(dropout(dropout_rate))
-    for i in range(n_dense - 1):
-        model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
-        model.add(Activation(activation))
-        model.add(GaussianNoise(0.01))
-        model.add(dropout(dropout_rate))
-
-    model.add(Dense(num_classes))
-    model.add(Activation(activation))
-    model.compile(loss='mae',
-                  metrics=['mae'],
-                  optimizer=optimizer)
-
-    return model
-
-
+model = load_model('./tmp.hdf5')
 X = pd.read_csv('./info/forex_signals_clean.csv', header=None)
 y = pd.read_csv('./info/USD_NTD_Rate_Clean.csv', header=None)
 train_data, test_data, train_targets, test_targets = train_test_split(X, y, test_size=0.2, random_state=4)
@@ -64,7 +29,7 @@ for i in range(k):
         [train_targets[: i * num_val_samples],
          train_targets[(i + 1) * num_val_samples:]],
         axis=0)
-    model = build_model()
+    model =  load_model('./tmp.hdf5')
     history = model.fit(partial_train_data,
                         partial_train_targets,
                         validation_data=(val_data, val_targets),
@@ -75,31 +40,21 @@ for i in range(k):
     all_mae_histories.append(mae_history)
     average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
 
-# +++++++++++++++++ callback
-model_ckpt = ModelCheckpoint(filepath='./tmp.hdf5',
-                             monitor='val_mean_absolute_error',
-                             save_best_only=False,
-                             mode='auto')
-
 # +++++++++++++++++ triaining final model
-model = build_model()
 model.summary()
 model.fit(train_data,
           train_targets,
-          epochs=500,
+          epochs=20000,
           batch_size=16,
-          verbose=1,
-          callbacks=[model_ckpt])
+          verbose=1,)
 
 test_mae, _ = model.evaluate(test_data, test_targets)
 print('Test MAE:', test_mae)
 
-pred_final = load_model('./tmp.hdf5')
 pred_loadback = model.predict(test_data)
 print(pred_loadback)
 # +++++++++++++++++ matplotlib
 plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
 plt.xlabel('Epochs')
 plt.ylabel('Validation Mae')
-plt.savefig('./info/training_result_mae.png')
-
+plt.savefig('./info/training_result_mae_final02.png')
